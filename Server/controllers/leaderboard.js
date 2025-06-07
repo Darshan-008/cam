1dd2155eb182083fd62c808fcd4e1a90b43b5d0b
Darshan-008 <darshandachu888@gmail.com>
2025-06-07 19:32:10 +0530

    Add ESLint flat config for server

--- Server/controllers/leaderboard.js
+++ Server/controllers/leaderboard.js
@@ -59,12 +59,3 @@ export const getLeaderboard = async (req, res) => {
         });
     }
 };
-            { $limit: 100 }
-        ]);
-
-        res.status(200).json(users);
-    } catch (error) {
-        console.error('Error fetching leaderboard:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-};
