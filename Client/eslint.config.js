f37d5a2ace4c56dd46780d2bb812d0795ef388df
Darshan-008 <darshandachu888@gmail.com>
2025-06-07 19:10:36 +0530

    fix the error

---
+++ Client/eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    plugins: {
+      react: require("eslint-plugin-react"),
+    },
+    rules: {
+      // Add your rules here
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
