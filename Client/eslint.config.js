a7cef06b9f7a24e517aaf353bbda0d5a1eb14a2d
Darshan-008 <darshandachu888@gmail.com>
2025-06-07 19:15:11 +0530

    fix the error in config"

--- Client/eslint.config.js
+++ Client/eslint.config.js
@@ -1,15 +1,27 @@
+import eslintPluginReact from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser";
+
 export default [
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
-      react: require("eslint-plugin-react"),
+      react: eslintPluginReact,
     },
     rules: {
-      // Add your rules here
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
     },
